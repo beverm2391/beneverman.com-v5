ebfe64743cc629865b61a7cd5b4a0051b71bdbbe
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-02-19 15:22:29 -0500

    still working through mdx crap

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [X] fix the slug prop in `blog/[slug...[]` because app dir doesn't use `getSaticPaths`
 - [ ] add `ExternalLink` component to mdx
 - [ ] test all gfm features
-- [ ] test Katex rendering
+- [ ] get the `$$` delimiter working by trying [memoized react markdown](https://github.com/beverm2391/chat-beverm-frontend/blob/74ff52bc6350c05733f1cd241909edda618739ea/components/markdown.tsx)
 - [ ] fix styling and layout
   - [ ] main layout
     - [ ] navbar desktop
--- app/blog/[slug]/components/PostBodyClient.tsx
+++ app/blog/[slug]/components/PostBodyClient.tsx
@@ -9,9 +9,11 @@ import ClientComponents from '@/core/components/MDX/ClientComponents';
 // remark
 import remarkGfm from 'remark-gfm'
 import remarkToc from 'remark-toc'
+import remarkMath from 'remark-math'
 
 // rehype
 import rehypeSlug from 'rehype-slug'
+
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypePrettyCode from 'rehype-pretty-code';
 import rehypeKatex from 'rehype-katex';
@@ -32,16 +34,15 @@ export async function PostBodyClient({ children }: { children: string }) {
             remarkPlugins: [
                 remarkGfm,
                 // remarkToc,
+                remarkMath,
             ],
             rehypePlugins: [
-                // @ts-ignore
+                // @ts-ignore (vFile version mismatch I think)
                 rehypeKatex,
                 // rehypeSlug,
                 rehypeAutolinkHeadings,
-                // @ts-ignore
-                // seems to have a conflicting dependency with something
                 [
-                    // @ts-ignore
+                    // @ts-ignore  (vFile version mismatch I think)
                     rehypePrettyCode,
                     {
                         theme: {
@@ -69,9 +70,6 @@ export async function PostBodyClient({ children }: { children: string }) {
     )
 
     return (
-        <>
-            {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css" integrity="sha384-n8MVd4RsNIU0tAv4ct0nTaAbDJwPJzDEaqSD1odI+WdtXRGWt2kTvGFasHpSy3SV" crossOrigin="anonymous"></link> */}
-            <MDXRemote {...source} components={MDXComponents} />
-        </>
+        <MDXRemote {...source} components={MDXComponents} />
     )
 }
