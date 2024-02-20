71c1649c6e0473de36afa1cf54e7099453606342
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-02-20 15:06:44 -0500

    forgot to del import

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,8 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getPost, getBlogPosts } from '@/lib/blog'
 import { PostBodyServer } from './components/PostBodyServer';
-import { PostBodyClient } from './components/PostBodyClient';
-import { PostBodyReactMarkdown } from './components/PostBodyReactMarkdown';
 
 export default async function PostPage({ params }: { params: { slug: string; } }) {
     // console.log("slug passed to page: ", params.slug)
