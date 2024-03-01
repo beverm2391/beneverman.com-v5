201196a27698e35f56d45f54b31a83c6a9c28487
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-03-01 11:15:55 -0500

    gonna work on bookshelf

--- core/components/Logo.tsx
+++ core/components/Logo.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 export default function Logo() {
     return (
         <div className='flex items-center justify-center'>
-            <Link href='/' className='hover:bg-gray-200 hover:cursor-pointer transition-all'>
-                <h1 className='text-5xl font-light uppercase'>ben.</h1>
+            <Link href='/' className='hover:bg-gray-200 hover:cursor-pointer transition-all px-2 py-1'>
+                <h1 className='text-4xl uppercase font-medium'>ben.</h1>
             </Link>
         </div>
     )
